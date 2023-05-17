354f3191f05e0957d76a30eb6789b341e78dc034
Pranjali Ramesh Borate <131251840+pranjaliborate@users.noreply.github.com>
2023-05-17 09:44:15 +0530

    Add files via upload

---
+++ git/mysql table.sql
@@ -0,0 +1,28 @@
+create table Onlinebooking (
+bookingNo int primary key,
+Bookingname varchar(30) not null,
+loc varchar(30));
+
+
+create table Airway(
+wayNo int primary key,
+wayname varchar(30),
+wayloc varchar(20)
+);
+
+
+create table customer(
+custno int primary key,
+custName varchar(20),
+cusloc varchar(20)
+);
+
+ create table banking (
+ bnkgidno int primary key,
+ bankname varchar(20),
+ bankloc varchar(20)
+ );
+ 
+ 
+
+
